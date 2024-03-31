2262cdc8e1d490a968134af67867c69ebcacb323
Siddharth Dhanuka <nonsense171@gmail.com>
2024-03-30 23:13:54 -0400

    bug fixes to get cloud functions running

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import json
 import os
 import requests
 import pymysql
+import functions_framework
 
 def prepare_email(to_email):
     api_key = os.environ.get('MAILGUN_API_KEY')
     domain = os.environ.get('MAILGUN_DOMAIN')
 
     message = {
-        "from": f"Your Name <postmaster@{domain}>",
+        "from": f"no-reply <postmaster@{domain}>",
         "to": [to_email, "nonsense171@gmail.com"],
         "subject": "BlogApp Verify Email Address",
         "text": f"Hello, please verify your email address by clicking on this link: http://{domain}:5000/verify/{to_email}"
@@ -27,7 +28,7 @@ def update_user_record(email):
         host = os.environ.get('DB_HOST')
         user = os.environ.get('DB_USER')
         password = os.environ.get('DB_PASSWORD')
-        database = os.environ.get('DATABASE')
+        database = os.environ.get('DB_DATABASE')
 
         conn = pymysql.connect(host=host, user=user, password=password, database=database)
         
@@ -40,12 +41,14 @@ def update_user_record(email):
     finally:
         conn.close()
 
-def send_email(event, context):
-    pubsub_message = event
+@functions_framework.cloud_event
+def send_email(cloud_event, context):
+    pubsub_message = cloud_event
     message_data = base64.b64decode(pubsub_message.data).decode('utf-8')
 
     message_json = json.loads(message_data)
     email_address = message_json.get('email')
+    email_address = "nonsense171@gmail.com"
 
     if email_address:
         prepare_email(email_address)
